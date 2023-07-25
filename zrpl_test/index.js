d4e1fac1dd3191d9dee0cb6a54c85f9eaed48376
SudarshanSirsi <sudarshanhegde247@gmail.com>
2023-07-25 13:05:54 +0530

    new test

--- zrpl_test/index.js
+++ zrpl_test/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 
-//app.use(express.static(path.join(__dirname, 'public')));  test test
+//app.use(express.static(path.join(__dirname, 'public')));  test test test
 app.use(express.static('public'));
 
 
